b1131dbf2a23c010d5bc467b2d301ae9d67e45d6
Bősze Máté <bosze.mate1997@gmail.com>
2021-01-17 14:41:31 +0100

    Cleanup and now all commands with after each other
    deleted commands from @client.event and replaced them with @bot.commands that can work simultaneously (good to check out $countdown and $meme).
    Example: start a countdown and while the countdown is going, send a request for a meme

--- ct4nk3r-meme-bot.py
+++ ct4nk3r-meme-bot.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 import random
+
 from discord.ext import commands
 
 from authentication import reddit_authentication
@@ -9,65 +10,45 @@ print("Authenticating discord bot...")
 TOKEN = discord_authentication()
 print("Authenticating reddit bot...")
 reddit = reddit_authentication()
-client = discord.Client()
+print("Reddit authentication as: {}".format(reddit.user.me()))
 subreddit = reddit.subreddit('memes')
-
-# 1
-from discord.ext import commands
-
-# 2
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='$')
 
 @bot.command(name='roll_dice', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice, number_of_sides):
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
     dice = [
         str(random.choice(range(1, number_of_sides + 1)))
         for _ in range(number_of_dice)
     ]
     await ctx.send(', '.join(dice))
+    pass
 
-@client.event
-async def on_ready():
-    print('Discord authentication as: {0.user}'.format(client))
+@bot.command(name='status', help='Change status of the bot' )
+async def status(ctx, program: str):
+    if program:
+        activity = discord.Game(name=program)
+        await bot.change_presence(activity=activity)
+        await ctx.reply('Status changed to: {}'.format(program))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command(name='meme', help='sends the newest meme into chat from r/memes')
+async def meme(ctx):
+    new_meme = subreddit.new(limit=1)
+    for meme in new_meme:
+        print(meme.url)
+        await ctx.reply(meme.url)
 
-    if message.content.startswith('!hello'):
-        await message.reply('Hello!', mention_author=True)
-
-    # bot = commands.Bot(command_prefix="$")
-    # bot = discord.Client()
-    # @bot.command()
-    # async def print(ctx, arg):
-    #     await ctx.channel.send(arg)
-
-
-    if message.content.startswith('!status'):
-        activity = discord.Game(name="with codes")
-        async def status(arg):
-            if arg:
-                activity = discord.Game(name = arg)        
-        await client.change_presence(activity=activity)
-
-    if message.content.startswith('!meme'):
-        new_meme_of_the_day = subreddit.new(limit=1)
-        for meme in new_meme_of_the_day:
-            print(meme.url)
-            await message.channel.send(meme.url)
-
-    if message.content.startswith('!test'):
-        await message.channel.send('test done')
-    
-    if message.content.startswith('!countdown'):
+@bot.command(name='countdown', help='counting down from 10 to 0')
+async def countdown(ctx):
         number = 10
-        msg = await message.channel.send(number)
+        msg = await ctx.reply(number)
         await asyncio.sleep(1.0)
         while (number != 0):
             number = number - 1
             await msg.edit(content=number)
             await asyncio.sleep(1.0)
 
-client.run(TOKEN)
+@bot.event
+async def on_ready():
+    print('Discord authentication as: {0.user}'.format(bot))
+
+bot.run(TOKEN)
