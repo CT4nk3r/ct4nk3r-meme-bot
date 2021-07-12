1dcace08723c10b2d97746fea4f252c7ed19dd8c
CT4nk3r <59850112+CT4nk3r@users.noreply.github.com>
2021-07-12 21:20:18 +0100

    Add files via upload

---
+++ authentication.py
@@ -0,0 +1,15 @@
+#authentication.py
+
+import praw
+def reddit_authentication():
+    print("Authenticating...")
+    reddit = praw.Reddit(client_id="client_id",
+                     client_secret="secret",
+                     username="username-meme-bot",
+                     password="password",
+                     user_agent="bot made by u/username")
+    return reddit
+
+def discord_authentication():
+    return 'DISCORD_TOKEN'
+#token should be at https://discord.com/developers/applications/00000000000000/bot -> token -> copy
